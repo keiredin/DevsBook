b20e3a817e4455596f9b1d8df0afe7a407da84dc
keiredin <61546450+keiredin@users.noreply.github.com>
2022-04-13 13:38:13 +0300

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -2,5 +2,3 @@ module.exports = {
     mongoURI: 'mongodb://localhost/devConnect',
     secretKey: "secret__key"
 }
-
-// //'mongodb+srv://firo:firasatlas@cluster0.0hfpx.mongodb.net/devConnect?retryWrites=true&w=majority'
